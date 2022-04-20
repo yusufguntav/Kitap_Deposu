db6fc3ce479a8138228f6d9a4b31962be571e426
YusufGuntav <ysf.guntav@gmail.com>
2022-04-20 22:19:32 +0300

    initial commit

---
+++ KitapDeposu/Bilgi_Giris_Musteri.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KitapDeposu
+{
+    public partial class Bilgi_Giris_Musteri : Form
+    {
+        public Bilgi_Giris_Musteri()
+        {
+            InitializeComponent();
+        }
+    }
+}
